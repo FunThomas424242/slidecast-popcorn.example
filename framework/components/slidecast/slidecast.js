546fa8c29958d5c0121e4487a7cf58d43633800c
FunThomas424242 <funthomas424242@gmail.com>
2017-04-16 22:00:15 +0200

    Verbindung zum Window Objekt hergestellt

--- framework/components/slidecast/slidecast.js
+++ framework/components/slidecast/slidecast.js
@@ -6,17 +6,17 @@
     modul.component('slidecast', {
         bindings: {
             titel: '@',
-            slides: '<'
+            slides: '<',
         },
         templateUrl: '../framework/components/slidecast/slidecast.html',
-        controller: function SlidecastController() {
+        controller: function SlidecastController( $window ) {
             var vm = this;
             vm.zeigeFolie = zeigeFolie;
             vm.getLastIndex = getLastIndex;
 
             this.hallo = function (text) {
-                alert("Titel: "+this.titel);
                 alert('Master Hallo' + text);
+                window.show(0);
             }
 
             function getLastIndex() {
@@ -26,7 +26,7 @@
             function zeigeFolie(folie) {
                 alert('Zeige Folie ' + folie);
             }
-        },
+        }
 
     });
 
