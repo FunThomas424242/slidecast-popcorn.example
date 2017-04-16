9c72238e2c82a1efd7be2194f03f5dfd7d0b5173
FunThomas424242 <funthomas424242@gmail.com>
2017-04-16 19:16:35 +0200

    temporärer stash

--- framework/components/slidecast/slidebar.js
+++ framework/components/slidecast/slidebar.js
@@ -7,21 +7,27 @@
     bindings: {
       slides: '<'
     },
-    controller:  function SlidecastController() {
+    controller: function SlidecastController() {
       var vm = this;
-      vm.testLog = testLog;
+      vm.zeigeFolie = zeigeFolie;
       vm.aktivieren = aktivieren;
-    
+      vm.getLastIndex = getLastIndex;
+
       aktivieren();
 
       function aktivieren() {
-        
+
       }
 
-      function testLog( value ) {
-        alert('Zeige Folie ' + value);
+      function getLastIndex() {
+        return this.slides.length - 1;
+      }
+
+      function zeigeFolie(folie) {
+        alert('Zeige Folie ' + folie);
       }
     },
+    controllerAs: "castCtrl"
 
   });
 
@@ -31,14 +37,74 @@
       container: '^slidecast'
     },
     controller: function SlidebarController() {
-      var vm = this;
-      vm.aktivieren = aktivieren;
-
-      aktivieren();
-
-      function aktivieren(){
-
+      this.$onInit = function () {
+        this.folie = 3;
+      };
+      this.$postLink = function(){
+        this.folie = 0;
       }
+
+      var vm = this;
+      vm.setzeAufErsteFolie = setzeAufErsteFolie;
+      vm.setzeAufLetzteFolie = setzeAufLetzteFolie;
+      vm.geheZurFolie = geheZurFolie;
+      vm.setzeAufVorherigeFolie = setzeAufVorherigeFolie;
+      vm.setzeAufNaechsteFolie = setzeAufNaechsteFolie;
+      vm.getFolienTitel = getFolienTitel;
+      vm.getLetztenFolienTitel = getLetztenFolienTitel;
+      vm.ermittleLetztenIndex = ermittleLetztenIndex;
+      vm.isSelectedFolie = isSelectedFolie;
+
+      function setzeAufErsteFolie() {
+        vm.folie = 0;
+        vm.geheZurFolie();
+      }
+
+      function setzeAufVorherigeFolie() {
+        var minIndex = 0;
+        vm.folie--;
+        if (vm.folie < minIndex) {
+          vm.folie = minIndex;
+        }
+        vm.geheZurFolie();
+      }
+
+      function setzeAufNaechsteFolie() {
+        var lastIndex = vm.ermittleLetztenIndex();
+        vm.folie++;
+        if (vm.folie > lastIndex) {
+          vm.folie = lastIndex;
+        }
+        vm.geheZurFolie();
+      }
+
+
+      function setzeAufLetzteFolie() {
+        vm.folie = vm.ermittleLetztenIndex();
+        vm.geheZurFolie();
+      }
+
+      function geheZurFolie() {
+        this.container.zeigeFolie(vm.folie);
+      }
+
+      function ermittleLetztenIndex() {
+        return this.container.getLastIndex();
+      }
+
+      function getFolienTitel(folienIndex) {
+        return this.container.slides[folienIndex][0];
+      }
+
+      function getLetztenFolienTitel() {
+        var lastIndex = vm.ermittleLetztenIndex();
+        return vm.getFolienTitel(lastIndex);
+      }
+
+      function isSelectedFolie(folienIndex) {
+        return vm.folie == folienIndex;
+      }
+
     }
 
   });
