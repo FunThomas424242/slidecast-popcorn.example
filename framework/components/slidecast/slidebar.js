3505bcd0f658c87c5d0b219e0c0a6270db25e090
FunThomas424242 <funthomas424242@gmail.com>
2017-04-17 13:27:42 +0200

    refactoring slidebar

--- framework/components/slidecast/slidebar.js
+++ framework/components/slidecast/slidebar.js
@@ -4,80 +4,64 @@
   var modul = angular.module('slidecastApp');
 
   modul.component('slidebar', {
-    templateUrl: '../framework/components/slidecast/slidebar.html',
+    bindings: {
+      first: '<',
+      prev: '<',
+      next: '<',
+      last: '<',
+      onFirst: '&',
+      onLast: '&',
+      onPrev: '&',
+      onNext: '&',
+      onSelect: '&',
+      onChange: '&'
+    },
     require: {
       container: '^slidecast'
     },
+    templateUrl: '../framework/components/slidecast/slidebar.html',
     controller: function SlidebarController() {
       this.$onInit = function () {
         this.folie = 0;
       };
       var vm = this;
-      vm.setzeAufErsteFolie = setzeAufErsteFolie;
-      vm.setzeAufLetzteFolie = setzeAufLetzteFolie;
-      vm.geheZurFolie = geheZurFolie;
-      vm.setzeAufVorherigeFolie = setzeAufVorherigeFolie;
-      vm.setzeAufNaechsteFolie = setzeAufNaechsteFolie;
-      vm.getFolienTitel = getFolienTitel;
-      vm.getLetztenFolienTitel = getLetztenFolienTitel;
-      vm.ermittleLetztenIndex = ermittleLetztenIndex;
-      vm.isSelectedFolie = isSelectedFolie;
+      vm.folie = 0;
+      vm.goToFirst = goToFirst;
+      vm.goToLast = goToLast;
+      vm.goToPrev = goToPrev;
+      vm.goToNext = goToNext;
+      vm.goToSelection = goToSelection;
 
-      function setzeAufErsteFolie() {
-        vm.folie = 0;
-        vm.geheZurFolie();
+
+      function goToFirst() {
+        vm.onFirst();
+        vm.folie = vm.onChange();
       }
 
-      function setzeAufVorherigeFolie() {
-        var minIndex = 0;
-        vm.folie--;
-        if (vm.folie < minIndex) {
-          vm.folie = minIndex;
-        }
-        vm.geheZurFolie();
+      function goToLast() {
+        vm.onLast();
+        vm.folie = vm.onChange();
       }
 
-      function setzeAufNaechsteFolie() {
-        var lastIndex = vm.ermittleLetztenIndex();
-        vm.folie++;
-        if (vm.folie > lastIndex) {
-          vm.folie = lastIndex;
-        }
-        vm.geheZurFolie();
+      function goToPrev() {
+        vm.onPrev();
+        vm.folie = vm.onChange();
       }
 
-
-      function setzeAufLetzteFolie() {
-        vm.folie = vm.ermittleLetztenIndex();
-        vm.geheZurFolie();
+      function goToNext() {
+        vm.onNext();
+        vm.folie = vm.onChange();
       }
 
-      function geheZurFolie() {
-        vm.container.zeigeFolie(vm.folie);
-      }
-
-      function ermittleLetztenIndex() {
-        return vm.container.getLastIndex();
-      }
-
-      function getFolienTitel(folienIndex) {
-        return vm.container.slides[folienIndex][0];
-      }
-
-      function getLetztenFolienTitel() {
-        var lastIndex = vm.ermittleLetztenIndex();
-        return vm.getFolienTitel(lastIndex);
-      }
-
-      function isSelectedFolie(folienIndex) {
-        return vm.folie == folienIndex;
+      function goToSelection(nr) {
+        vm.onSelect({ folienIndex: nr });
       }
 
     }
 
   });
 
-  
+
 
 
 })(window.angular);
--- framework/components/slidecast/slidecast.js
+++ framework/components/slidecast/slidecast.js
@@ -15,23 +15,69 @@
             this.$onInit = function () {
                 this.alleMedien = window[this.mediaArrayName];
                 this.alleFolien = window[this.slidesArrayName];
+                this.folie = 0;
             };
             var vm = this;
+            vm.getCurrentSlide = getCurrentSlide;
             vm.zeigeFolie = zeigeFolie;
             vm.getLastIndex = getLastIndex;
-
+            vm.setzeAufFolie = setzeAufFolie;
+            vm.setzeAufErsteFolie = setzeAufErsteFolie;
+            vm.setzeAufLetzteFolie = setzeAufLetzteFolie;
+            vm.setzeAufVorherigeFolie = setzeAufVorherigeFolie;
+            vm.setzeAufNaechsteFolie = setzeAufNaechsteFolie;
 
             vm.hallo = function (text) {
                 alert('Master Hallo' + text);
+            }
+
+            function getCurrentSlide(){
+                return this.folie;
+            }
+
+            function zeigeFolie(folienIndex) {
+                alert('Zeige Folie ' + folienIndex);
             }
 
             function getLastIndex() {
                 return vm.slides.length - 1;
             }
 
-            function zeigeFolie(folienIndex) {
-                alert('Zeige Folie ' + folienIndex);
+            function setzeAufFolie( folienIndex ){
+                vm.folie = folienIndex;
+                vm.zeigeFolie( vm.folie );
             }
+
+            function setzeAufErsteFolie() {
+                vm.folie = 0;
+                vm.zeigeFolie(vm.folie);
+            }
+
+            function setzeAufLetzteFolie() {
+                vm.folie = vm.getLastIndex();
+                vm.zeigeFolie(vm.folie);
+            }
+
+            function setzeAufVorherigeFolie() {
+                var minIndex = 0;
+                vm.folie--;
+                if (vm.folie < minIndex) {
+                    vm.folie = minIndex;
+                }
+                vm.zeigeFolie(vm.folie);
+            }
+
+            function setzeAufNaechsteFolie() {
+                var lastIndex = vm.getLastIndex();
+                vm.folie++;
+                if (vm.folie > lastIndex) {
+                    vm.folie = lastIndex;
+                }
+                vm.zeigeFolie(vm.folie);
+            }
+
+
+
         }
 
     });
